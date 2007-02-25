11e79a2ce27e8e6a9b75b9a7a1b0d46bd6eaba66
pot <pot@3cc57667-272a-0410-8af6-ba9847f13327>
2007-02-25 21:11:41 +0000

    fix formatting error

--- website/header.php
+++ website/header.php
@@ -24,7 +24,7 @@
 			<? include "lang/".$lang."/ihv.php"; ?>
 		</ul>
 		<hr />
-		<ul>
+		<ul class="navigation">
 			<li><a class="navigation" href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a></li>
 			<li><a class="navigation" href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" /></a></li>
 		</ul>
