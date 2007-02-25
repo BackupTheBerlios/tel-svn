4ffd799bf348649790d470a9353a248f6cd79c52
pot <pot@3cc57667-272a-0410-8af6-ba9847f13327>
2007-02-25 21:40:33 +0000

    swp-Dateien löschen News-Seite hinzugefügt News-Seite in IHV verlinkt

--- website/lang/de/ihv.php
+++ website/lang/de/ihv.php
@@ -1,2 +1,3 @@
 <li><a class="navigation" href="index.php?to=home">Übersicht</a></li>
 <li><a class="navigation" href="index.php?to=download">Download</a></li>
+<li><a class="navigation" href="index.php?to=news">News</a></li>
