88138e552c46b530722900328254f5d9f7936af4
pot <pot@3cc57667-272a-0410-8af6-ba9847f13327>
2007-02-26 11:00:53 +0000

    adding english version of the developer table

---
+++ website/lang/en/developer.php
@@ -0,0 +1,61 @@
+<h1>Kontaktiere uns</h1>
+<p>In the following table are the official tel developers. The GPG key you will find on one of the key server.</p>
+
+<p>
+<table>
+<tr>
+ <td>
+  Developer
+ </td>
+ <td>
+  Nick
+ </td>
+ <td>
+  Position
+ </td>
+ <td>
+  E-Mail
+ </td>
+ <td>
+  GPG
+ </td>
+</tr>
+
+<tr>
+ <td>
+  Sebastian Wiesner
+ </td>
+ <td>
+  lunar
+ </td>
+ <td>
+  tel developer, webmaster
+ </td>
+ <td>
+  basti.wiesner AT gmx DOT net
+ </td>
+ <td>
+  Yes
+ </td>
+</tr>
+
+<tr>
+ <td>
+  Remo Wenger
+ </td>
+ <td>
+  pot
+ </td>
+ <td>
+  project leader, webmaster
+ </td>
+ <td>
+  potrmwn AT gmail DOT com
+ </td>
+ <td>
+  Yes
+ </td>
+</tr>
+
+</table>
+</p>
