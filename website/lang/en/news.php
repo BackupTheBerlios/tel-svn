b8df7f302ebc15278698296f9741ecfa83b3ba1d
lunar <lunar@3cc57667-272a-0410-8af6-ba9847f13327>
2007-02-26 13:13:46 +0000

    Added english news page Updated english index

---
+++ website/lang/en/news.php
@@ -0,0 +1,2 @@
+<h1>Keep up to date</h1>
+<p>News are published on the <a href="http://developer.berlios.de/projects/tel/">tel project page on BerliOS</a>. At the moment we're trying to include these news into this page. Until this is done, you can get the news from this <a href="http://developer.berlios.de/export/projnews.php?group_id=8132&limit=6&flat=1&show_summaries=1">simple raw page</a>.</p>
