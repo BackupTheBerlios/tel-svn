b47069304e3388ce81d0d39a281d237ca9f0a3ca
lunar <lunar@3cc57667-272a-0410-8af6-ba9847f13327>
2007-02-26 13:47:56 +0000

    Updated changelog and version information

--- trunk/tel.py
+++ trunk/tel.py
@@ -22,7 +22,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE."""
 
-__version__ = '0.1.5-pre1'
+__version__ = '0.1.5'
 
 __authors__ = ['Sebastian Wiesner <basti.wiesner@gmx.net>']
 
