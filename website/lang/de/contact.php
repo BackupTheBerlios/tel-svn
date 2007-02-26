32b3b8e895a6a57d5faada4a05124d88e685e278
lunar <lunar@3cc57667-272a-0410-8af6-ba9847f13327>
2007-02-26 18:20:12 +0000

    Updated contact information to include gpg keys

--- website/lang/de/contact.php
+++ website/lang/de/contact.php
@@ -1,14 +1,16 @@
 <h1>Kontaktiere uns</h1>
-<p>Folgend eine Liste von den offiziellen tel-Entwickler. Den GPG Schlüssel findest du, wenn vorhanden, auf einem der Key-Server.</p>
+<p>Folgend eine Liste von den offiziellen tel-Entwickler.</p>
 
 <p>
 Sebastian "lunar" Wiesner<br />
 tel-Entwickler, Webmaster<br />
 basti DOT wiesner AT gmx DOT net<br />
+GPG key: 0x82DE71BE auf hkp://wwwkeys.eu.gpg.net<br />
 </p>
 
 <p>
 Remo "pot" Wenger<br />
 Projekt Leiter, Webmaster<br />
 potrmwn AT gmail DOT com<br />
+GPG Schlüssel: 0xB6D4FFA0 auf hkp://wwwkeys.eu.gpg.net<br />
 </p>
--- website/lang/en/contact.php
+++ website/lang/en/contact.php
@@ -1,14 +1,16 @@
 <h1>Contact us</h1>
-<p>These are the developers of tel. GPG keys can be found on the keyservers.</p>
+<p>These are the developers of tel.</p>
 
 <p>
 Sebastian "lunar" Wiesner<br />
 tel developer, webmaster<br />
 basti DOT wiesner AT gmx DOT net<br />
+GPG key: 0x82DE71BE on hkp://wwwkeys.eu.gpg.net<br />
 </p>
 
 <p>
 Remo "pot" Wenger<br />
 project leader, webmaster<br />
 potrmwn AT gmail DOT com<br />
+GPG key: 0xB6D4FFA0 on hkp://wwwkeys.eu.gpg.net<br />
 </p>
