e340b426fbc0bd24f713b1b8cb34884378561535
lunar <lunar@3cc57667-272a-0410-8af6-ba9847f13327>
2007-02-27 15:48:46 +0000

    Added navigation link to BerliOS project page

--- website/lang/de/ihv.php
+++ website/lang/de/ihv.php
@@ -3,3 +3,4 @@
 <li><a class="navigation" href="index.php?to=news">News</a></li>
 <li><a class="navigation" href="index.php?to=contact">Kontakt</a></li>
 <li><a class="navigation" href="index.php?to=help">Tel unterstützen</a></li>
+<li><a class="navigation" href="http://developer.berlios.de/projects/tel">BerliOS Projektseite</a></li>
--- website/lang/en/ihv.php
+++ website/lang/en/ihv.php
@@ -3,3 +3,4 @@
 <li><a class="navigation" href="index.php?to=news">News</a></li>
 <li><a class="navigation" href="index.php?to=contact">Contact</a></li>
 <li><a class="navigation" href="index.php?to=help">Support tel</a></li>
+<li><a class="navigation" href="http://developer.berlios.de/projects/tel">BerliOS project page</a></li>
