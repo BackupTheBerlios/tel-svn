fc5c2f6fa53af771e8d983e8996b3957cf79f01b
lunar <lunar@3cc57667-272a-0410-8af6-ba9847f13327>
2007-02-27 15:50:39 +0000

    Fixed list on help page

--- website/lang/de/help.php
+++ website/lang/de/help.php
@@ -2,7 +2,7 @@
 
 <p>Falls du daran interessiert bist, bei der Entwicklung von tel mitzuhelfen, gibt es viele Möglichkeiten, auf denen sowohl erfahrene als auch unerfahrene Benutzer helfen können:</p>
 
-<li>
+<ul>
 <li>Du kannst einfach tel testen und uns alle noch unbekannten Fehler, die du findest, berichten.</li>
 <li>Wenn du ein erfahrener Benutzer bist, kannst du anderen auf der <a href="http://lists.berlios.de/mailman/listinfo/tel-users">tel Nutzer Mailingliste</a> helfen.</li>
 <li>Du könntest beim Schreiben einer Dokumentation helfen.</li>
@@ -10,7 +10,7 @@
 <li>Du könntest in unsere Arbeit reinschauen, indem du die<a href="http://lists.berlios.de/mailman/listinfo/tel-dev">Entwickler-Mailingliste</a> liest oder <a href="index.php?to=download">die aktuellste Revision von tel runterlädst</a> und dort ein bisschen reinschnupperst.</li>
 <li>Helfe tel, indem du es (international) bekannt machst und anderen vorführst</li>
 <li>Du kannst dir die ausstehenden Aufgaben im <a href="http://developer.berlios.de/feature/?group_id=8132">tel Feature Manager</a> ansehen und dabei helfen, diese zu implementieren.
-</ol>
+</ul>
 
 <p>
 Es gibt viele Arten zu helfen und nur wenige erfordern gute Programmierkenntnisse. Wenn du Fragen oder Probleme hast oder am Projekt teilnehmen willst, <a href="index.php?to=contact">kontaktiere uns</a> ungeniert oder schreibe auf unserer <a href="http://lists.berlios.de/mailman/listinfo/tel-users">Mailingliste.</a>.
--- website/lang/en/help.php
+++ website/lang/en/help.php
@@ -2,7 +2,7 @@
 
 <p>If you are interested in supporting tel, there are a lot of ways to help for both unexperienced and experienced people:</p>
 
-<li>
+<ul>
 <li>You can test tel and report all bugs and failures you find!</li>
 <li>If you are an experienced user, you can help other on the <a href="http://lists.berlios.de/mailman/listinfo/tel-users">tel users mailing list</a>.</li>
 <li>You could help on documenting tel.</li>
@@ -10,7 +10,7 @@
 <li>You could dig into our work by reading the <a href="http://lists.berlios.de/mailman/listinfo/tel-dev">developer mailing list</a> or <a href="index.php?to=download">grapping the latest revision from Subversion</a>.</li>
 <li>Support tel by showing it to your friends and making it known to the world!</li>
 <li>You can look at open feature requests in the <a href="http://developer.berlios.de/feature/?group_id=8132">tel feature manager</a> and help implementing them.
-</ol>
+</ul>
 
 <p>
 There are lots of ways to to help and only few require programming knowledge. If you have questions or problems or want to participate, just <a href="index.php?to=contact">contact us</a> or write on our <a href="http://lists.berlios.de/mailman/listinfo/tel-users">mailing list.</a>.
