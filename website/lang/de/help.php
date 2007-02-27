3f93f593c6baac833322f3c2e0f1065762b75793
pot <pot@3cc57667-272a-0410-8af6-ba9847f13327>
2007-02-27 11:27:59 +0000

    removing <p> and </p>-tags because there are already <ol> and </ol>

--- website/lang/de/help.php
+++ website/lang/de/help.php
@@ -2,7 +2,6 @@
 
 <p>Falls du daran interessiert bist, bei der Entwicklung von tel mitzuhelfen, gibt es viele Möglichkeiten, auf denen sowohl erfahrene als auch unerfahrene Benutzer helfen können:</p>
 
-<p>
 <ol>
 <li>Du kannst einfach tel und die bald mitgelieferten Skripte testen und alle noch unbekannten Fehler, die du findest, uns zu berichten</li>
 <li>Wenn du ein erfahrener Benutzer bist, kannst du anderen durch die <a href="https://lists.berlios.de/mailman/listinfo/tel-users">Mailinglisten</a> helfen</li>
@@ -12,7 +11,6 @@
 <li>Du könntest in unsere aktuellen Arbeiten reinschauen, indem du unsere <a href="https://lists.berlios.de/mailman/listinfo/tel-dev">Developer-Mailingliste</a> liest oder <a href="index.php?to=download">die aktuellste Revision von tel runterlädst</a> und dort ein bisschen reinschnupperst</li>
 <li>Helfe tel, indem du es (international) bekannt machst und anderen vorführst</li>
 </ol>
-</p>
 
 <p>
 Es gibt viele Arten, sich bei einem Projekt zu beteiligen und nur wenige erfordern gute Programmierkenntnisse. Wenn du Fragen oder Probleme hast, <a href="index.php?to=contact">kontaktiere uns</a> ungeniert oder schreibe in unsere <a href="https://lists.berlios.de/mailman/listinfo/tel-users">Mailingliste</a>.
