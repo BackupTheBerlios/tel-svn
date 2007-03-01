ce80c57522ee8e943b9408a47021a3db717c8259
pot <pot@3cc57667-272a-0410-8af6-ba9847f13327>
2007-03-01 18:55:10 +0000

    adding forgotten entry in ihv change h2 format in stylesheet

--- website/lang/de/ihv.php
+++ website/lang/de/ihv.php
@@ -1,5 +1,6 @@
 <li><a class="navigation" href="index.php?to=home">Übersicht</a></li>
 <li><a class="navigation" href="index.php?to=download">Download</a></li>
+<li><a class="navigation" href="index.php?to=screenshots">Screenshots</a></li>
 <li><a class="navigation" href="index.php?to=news">News</a></li>
 <li><a class="navigation" href="index.php?to=contact">Kontakt</a></li>
 <li><a class="navigation" href="index.php?to=help">Tel unterstützen</a></li>
--- website/lang/en/ihv.php
+++ website/lang/en/ihv.php
@@ -1,5 +1,6 @@
 <li><a class="navigation" href="index.php?to=home">Home</a></li>
 <li><a class="navigation" href="index.php?to=download">Download</a></li>
+<li><a class="navigation" href="index.php?to=screenshots">Screenshots</a></li>
 <li><a class="navigation" href="index.php?to=news">News</a></li>
 <li><a class="navigation" href="index.php?to=contact">Contact</a></li>
 <li><a class="navigation" href="index.php?to=help">Support tel</a></li>
