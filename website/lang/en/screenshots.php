3c39a9f6d3154637820e3d307cc51855106df75f
pot <pot@3cc57667-272a-0410-8af6-ba9847f13327>
2007-03-01 18:56:38 +0000

    translate german into english

--- website/lang/en/screenshots.php
+++ website/lang/en/screenshots.php
@@ -1,5 +1,5 @@
-<h1>tel ist sexy</h1>
+<h1>tel is sexy</h1>
 
-<h2>editieren</h2>
-<p>Mit tel ist es sehr einfach, ein vorhandener Eintrag zu bearbeiten</p>
+<h2>edit</h2>
+<p>In tel it is very easy to edit an entry</p>
 <p><img src="images/screenshots/edit_entry.png" alt="edit entry"></p>
