38ff32ec179f87f8016a5aed774db9eb07930dbc
lunar <lunar@3cc57667-272a-0410-8af6-ba9847f13327>
2007-03-01 19:20:15 +0000

    Improved and completed english translation Fixed a typo Removed unnecessary links from home.php (which makes the thing more readable)

--- website/lang/en/home.php
+++ website/lang/en/home.php
@@ -1,12 +1,12 @@
 <h1>What is tel?</h1>
 <p>tel is a little console-based phone book program. It allows adding, modifing, editing and searching of phone book entries right on your terminal. Pretty printing capabilites are also provided.</p>
 <p>Entries are stored in simple csv file. This eases import and export with common spread sheet applications like Microsoft Excel or OpenOffice.org Calc.</p>
-<p>tel is written in <a href="http://www.python.org">Python</a> and currently developed by Sebastian "lunar" Wiesner. It is distributed under the <a href="http://www.opensource.org/licenses/mit-license.php">MIT license</a> and therefore free software. You can download it free of charge, share it with your friends, modify and even sell it.</p>
+<p>tel is written in <a href="http://www.python.org">Python</a> and distributed under the <a href="http://www.opensource.org/licenses/mit-license.php">MIT license</a> and therefore free software. You can download it free of charge, share it with your friends, modify and even sell it.</p>
 
 <h1>History</h1>
 <p>Originally tel was a little <a href="http://druckian.hopto.org/~remo/tel/">bash script</a> developed by <a href="http://www.ubuntuusers.de/users/Pot/">Pot</a> and <a href="http://www.ubuntuusers.de/users/sabsirro/">sabsirro</a> in the german Ubuntu forum <a href="http://forum.ubuntuusers.de/topic/70328/">ubuntuusers.de</a>. With development continuing at quick pace, bash quickly proved to be insufficient for writing such large projects as tel had now become.</p>
 
-<p>First <a href="http://www.ubuntuusers.de/users/Pot/">Pot</a> and <a href="http://www.ubuntuusers.de/users/sabsirro/">sabsirro</a> planned to rewrite tel in Perl. But before development in perl had begun, <a href="http://www.ubuntuusers.de/users/lunar/">Lunar</a> <a href="http://forum.ubuntuusers.de/goto?post=589400">came up</a> with a <a href="http://www.python.org">Python</a> version of tel. This version was met with great interest. After a little while <a href="http://www.ubuntuusers.de/users/Pot/">Pot</a> and <a href="http://www.ubuntuusers.de/users/lunar/">Lunar</a> agreed to continue this version as a separate project on <a href="http://developer.berlios.de">BerliOS</a>.</p>
+<p>First Pot and sabsirro planned to rewrite tel in Perl. But before development in perl had begun, <a href="http://www.ubuntuusers.de/users/lunar/">lunar</a> <a href="http://forum.ubuntuusers.de/goto?post=589400">came up</a> with a <a href="http://www.python.org">Python</a> version of tel. This version was met with great interest. After a little while Pot and lunar agreed to continue this version as a separate project on <a href="http://developer.berlios.de">BerliOS</a>. When sabsirro joined the project after a little while, the tel developer team was complete.</p>
 
 <h1>Have fun!</h1>
 <p>Give tel a try!</p>
