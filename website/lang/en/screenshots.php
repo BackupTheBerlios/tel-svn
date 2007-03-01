e05aa2446f1f5a055f7300471af56e744ccfb337
pot <pot@3cc57667-272a-0410-8af6-ba9847f13327>
2007-03-01 20:31:14 +0000

    fixed a typo change formatted h2 in stylesheet

--- website/lang/en/screenshots.php
+++ website/lang/en/screenshots.php
@@ -1,5 +1,5 @@
 <h1>tel is sexy</h1>
 
 <h2>edit</h2>
-<p>Editing an entry withtel is really easy:</p>
+<p>Editing an entry with tel is really easy:</p>
 <p><img src="images/screenshots/edit_entry.png" alt="edit entry"></p>
