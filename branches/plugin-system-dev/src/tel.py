a3556270e1a3214807aa762cbbf09d8ef48a07a4
lunar <lunar@3cc57667-272a-0410-8af6-ba9847f13327>
2007-03-31 14:05:20 +0000

    Merged changes from r122 and fixed version information

--- branches/plugin-system-dev/src/tel.py
+++ branches/plugin-system-dev/src/tel.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 # main module
 
-
 """Contains globale configuration and provides the starter script for tel.
 
 :mvar config: Stores a global configuration object, which provides access to
@@ -31,7 +30,8 @@ THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE."""
-__version__ = '0.1.7-pre1'
+
+__version__ = '0.2.0-pre1'
 __authors__ = ('Sebastian Wiesner <basti.wiesner@gmx.net>',
                'Remo Wenger <potrmwn@gmail.com>')
 
