2ea54b1171f2f31960a75b43ce90451264f7c2ad
lunar <lunar@3cc57667-272a-0410-8af6-ba9847f13327>
2007-06-05 08:18:39 +0000

    Added global attributes for stream encodings Moved redirection code into a function Added stdin encoding

--- branches/plugin-system-dev/src/tel/encodinghelper.py
+++ branches/plugin-system-dev/src/tel/encodinghelper.py
@@ -26,10 +26,19 @@ import codecs
 import subprocess
 
 
-# Only execute _outside_ idle
-if "idlelib" not in sys.modules:
-    # determine out_enc from standard values
-    out_enc = (getattr(sys.stdout, "encoding", None) or
+def _read_locale():
+    """Asks the 'locale' command on unix like systems"""
+    process = subprocess(['locale'], stdout=subprocess.PIPE)
+    process.wait()
+    for line in process.stdout:
+        line = line.strip()
+        if line.startswith('LC_MESSAGES'):
+            return line.split('=')[1]
+
+
+def _get_encoding(outputstream):
+    """Tries to determine encoding of standard output"""
+    out_enc = (getattr(outputstream, "encoding", None) or
                sys.getfilesystemencoding())
     if not out_enc:
         # if it's still not identified
@@ -38,52 +47,26 @@ if "idlelib" not in sys.modules:
             out_enc = "cp850"
         elif any((plat.startswith(p) for p in ("linux", "aix"))):
             # try to read locale output
-            process = subprocess(['locale'], stdout=subprocess.PIPE)
-            process.wait()
-            for line in process.stdout:
-                line = line.strip()
-                if line.startswith('LC_MESSAGES'):
-                    out_enc = line.split('=')[1]
-                    break              
+
         elif plat.startswith("cygwin"):
             out_enc = "raw_unicode_escape"
         else:
             out_enc = ""
     if out_enc.upper() in ("ASCII", "US-ASCII", "ANSI_X3.4-1968", "POSIX"):
         out_enc = ""
-    if out_enc:
-        sys.stdout = codecs.getwriter(out_enc)(sys.stdout)
+    return out_enc
 
-    # TODO: Do we need this?
-    ##
-    ## STDIN
-    ##
-    #if _stdin_encoding.lower() == "none":
-        #pass
-    #elif _stdin_encoding.lower() == "auto":
-        ## sys.stdin umleiten, damit Umlaute korrekt eingelesen werden.
-        ## Dafür wird zuerst herausgefunden mit welcher Platform man es zu tun hat und
-        ## danach wird, abhängig von der Platform, versucht das Encoding heraus zu finden.
-        #in_enc = ""
-        #plat = sys.platform
-        #if plat.startswith("win"):
-            #in_enc = getattr(sys.stdin, "encoding", None) or \
-                #sys.getfilesystemencoding() or "mbcs"
-            ##if in_enc == "mbcs":
-            ##    in_enc = "cp850"
-        #elif _istrue(plat.startswith, ("linux", "aix")):
-            #in_enc = getattr(sys.stdin, "encoding", None) or \
-                #sys.getfilesystemencoding() or \
-                #os.popen("locale | grep 'LC_MESSAGES'").read().strip().split("=")[1]
-        #elif _istrue(plat.startswith, ("cygwin",)):
-            #in_enc = getattr(sys.stdin, "encoding", None) or \
-                #sys.getfilesystemencoding() or "raw_unicode_escape"
-        #else:
-            #in_enc = getattr(sys.stdin, "encoding", None) or \
-                #sys.getfilesystemencoding() or ""
-        #if in_enc.upper() in ("ASCII", "US-ASCII", "ANSI_X3.4-1968", "POSIX"):
-            #in_enc = ""
-        #if in_enc:
-            #sys.stdin = codecs.getreader(in_enc)(sys.stdin)
-    #else:
-        #sys.stdin = codecs.getreader(_stdin_encoding)(sys.stdin)
+
+stdout_encoding = _get_encoding(sys.stdout)
+stdin_encoding = _get_encoding(sys.stdin)
+
+
+def redirect_stream():
+    """Redirects output stream"""
+    # Only execute _outside_ idle
+    if "idlelib" not in sys.modules:
+        # redirect standard output stream
+        if stdout_encoding:
+            sys.stdout = codecs.getwriter(stdout_encoding)(sys.stdout)
+        if stdin_encoding:
+            sys.stdin = codecs.getreader(stdin_encoding)(sys.stdin)
