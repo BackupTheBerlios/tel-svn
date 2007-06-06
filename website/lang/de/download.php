302d3efca3d7305a17add1be2b48366c53e2bff0
pot <pot@3cc57667-272a-0410-8af6-ba9847f13327>
2007-06-06 18:08:10 +0000

    Fixed Tel to tel (I don't want to be strict, but it's important for me)

--- website/lang/de/download.php
+++ website/lang/de/download.php
@@ -2,4 +2,4 @@
 <p>Wenn du eine stabile, einigermaßen gut getestet Version von tel verwenden willst, dann solltest du ein offizielles Release herunterladen. Die offiziellen Releases findest du im <a href="http://developer.berlios.de/project/showfiles.php?group_id=8132">Download-Bereich</a> von BerliOS.</p>
 
 <h1>Subversion</h1>
-<p>Wenn du immer die neueste Version von tel verwenden möchtest, keine Angst vor Bugs und Fehler hast oder vielleicht sogar an der Entwicklung von Tel interessert bist, dann kannst du dir Tel aus dem <a href="http://subversion.tigris.org/">Subversion</a>-Repository <a href="http://developer.berlios.de/svn/?group_id=8132">herunterladen</a> .</p>
+<p>Wenn du immer die neueste Version von tel verwenden möchtest, keine Angst vor Bugs und Fehler hast oder vielleicht sogar an der Entwicklung von tel interessert bist, dann kannst du dir tel aus dem <a href="http://subversion.tigris.org/">Subversion</a>-Repository <a href="http://developer.berlios.de/svn/?group_id=8132">herunterladen</a> .</p>
