da91bc11ab25ca46521f60efddb73631abfa8162
lunar <lunar@3cc57667-272a-0410-8af6-ba9847f13327>
2007-06-09 17:10:13 +0000

    Replaces raw_input with a unicode-capable implementation

--- branches/plugin-system-dev/src/tel/encodinghelper.py
+++ branches/plugin-system-dev/src/tel/encodinghelper.py
@@ -61,11 +61,25 @@ stdout_encoding = _get_encoding(sys.stdout)
 stdin_encoding = _get_encoding(sys.stdin)
 
 
-def redirect_std_streams():
-    """Redirects output stream.
+_no_encoding_raw_input = raw_input
 
+def raw_input(prompt=''):
+    """A raw input function, that returns unicode objects decoded according
+    to stdin_encoding"""
+    value = _no_encoding_raw_input(prompt)
+    return value.decode(stdin_encoding)
+    
+
+def redirect_std_streams(replace_raw_input=True):
+    """Redirects output stream.
+    
     **Note**: sys.stdin isn't currently redirected, as this breaks with
-    raw_input"""
+    raw_input. As a workaround you can set `replace_raw_input` to True,
+    which will replace raw_input with an implementation, that returns
+    unicode strings.
+
+    The original raw_input function will still be available from this module
+    as _no_encoding_raw_input"""
     # Only execute _outside_ idle
     if "idlelib" not in sys.modules:
         # redirect standard output stream
@@ -73,3 +87,9 @@ def redirect_std_streams():
             sys.stdout = codecs.getwriter(stdout_encoding)(sys.stdout)
         ## if stdin_encoding:
         ##     sys.stdin = codecs.getreader(stdin_encoding)(sys.stdin)
+        if replace_raw_input:
+            import __builtin__
+            __builtin__.__dict__['raw_input'] = raw_input
+            
+
+        
