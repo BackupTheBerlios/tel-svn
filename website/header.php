110c80a945551f5bb6622d449823ebb265ba3e27
pot <pot@3cc57667-272a-0410-8af6-ba9847f13327>
2007-06-14 16:49:46 +0000

    adjusted lang/{de,en}/ihv.php and header.php for new design temporary removed w3c's validator images

--- website/header.php
+++ website/header.php
@@ -42,14 +42,7 @@
 	</div>
 	
 	<div id="ihv">
-		<ul class="navigation">
-			<? include "lang/".$lang."/ihv.php"; ?>
-		</ul>
-		<hr />
-		<ul class="navigation">
-			<li><a class="navigation" href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a></li>
-			<li><a class="navigation" href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" /></a></li>
-		</ul>
+        <? include "lang/".$lang."/ihv.php"; ?>
 	</div>
 
 	<div id="index">
--- website/lang/en/ihv.php
+++ website/lang/en/ihv.php
@@ -1,7 +1,7 @@
-<li><a class="navigation" href="index.php?to=home">Home</a></li>
-<li><a class="navigation" href="index.php?to=download">Download</a></li>
-<li><a class="navigation" href="index.php?to=screenshots">Screenshots</a></li>
-<li><a class="navigation" href="index.php?to=news">News</a></li>
-<li><a class="navigation" href="index.php?to=contact">Contact</a></li>
-<li><a class="navigation" href="index.php?to=help">Support tel</a></li>
-<li><a class="navigation" href="http://developer.berlios.de/projects/tel">BerliOS project page</a></li>
+<a class="navigation" href="index.php?to=home">Home</a>
+<a class="navigation" href="index.php?to=download">Download</a>
+<a class="navigation" href="index.php?to=screenshots">Screenshots</a>
+<a class="navigation" href="index.php?to=news">News</a>
+<a class="navigation" href="index.php?to=contact">Contact</a>
+<a class="navigation" href="index.php?to=help">Support tel</a>
+<a class="navigation" href="http://developer.berlios.de/projects/tel">BerliOS project page</a>
