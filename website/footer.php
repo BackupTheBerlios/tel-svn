8ffa00d605773e4dbaabd268ffd6a1b8fb68cdb2
pot <pot@3cc57667-272a-0410-8af6-ba9847f13327>
2007-06-26 16:53:02 +0000

    added w3c images (validations) to the footer

--- website/footer.php
+++ website/footer.php
@@ -1,4 +1,9 @@
 	</div>
 </div>
+<div id="w3cimages">
+<a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+<a href="http://jigsaw.w3.org/css-validator/"><img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss" 
+alt="Valid CSS!" /></a>
+</div>
 </body>
 </html>
