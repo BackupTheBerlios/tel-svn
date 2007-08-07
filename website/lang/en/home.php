a567c332e4af629d780a593318ae2041747e940b
pot <pot@3cc57667-272a-0410-8af6-ba9847f13327>
2007-08-07 06:25:29 +0000

    - temporarily removed link to pot's UU profile

--- website/lang/en/home.php
+++ website/lang/en/home.php
@@ -4,7 +4,7 @@
 <p>tel is written in <a href="http://www.python.org">Python</a> and distributed under the <a href="http://www.opensource.org/licenses/mit-license.php">MIT license</a> and therefore free software. You can download it free of charge, share it with your friends, modify and even sell it.</p>
 
 <h1>History</h1>
-<p>Originally tel was a little bash script developed by <a href="http://www.ubuntuusers.de/users/Pot/">Pot</a> and <a href="http://www.ubuntuusers.de/users/sabsirro/">sabsirro</a> in the german Ubuntu forum <a href="http://forum.ubuntuusers.de/topic/70328/">ubuntuusers.de</a>. With development continuing at quick pace, bash quickly proved to be insufficient for writing such large projects as tel had now become.</p>
+<p>Originally tel was a little bash script developed by Pot and <a href="http://www.ubuntuusers.de/users/sabsirro/">sabsirro</a> in the german Ubuntu forum <a href="http://forum.ubuntuusers.de/topic/70328/">ubuntuusers.de</a>. With development continuing at quick pace, bash quickly proved to be insufficient for writing such large projects as tel had now become.</p>
 
 <p>First Pot and sabsirro planned to rewrite tel in Perl. But before development in perl had begun, <a href="http://www.ubuntuusers.de/users/lunar/">lunar</a> <a href="http://forum.ubuntuusers.de/goto?post=589400">came up</a> with a <a href="http://www.python.org">Python</a> version of tel. This version was met with great interest. After a little while Pot and lunar agreed to continue this version as a separate project on <a href="http://developer.berlios.de">BerliOS</a>. When sabsirro joined the project after a little while, the tel developer team was complete.</p>
 
